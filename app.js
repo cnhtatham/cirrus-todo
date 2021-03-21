003d7e576ecb3b181d18a879c1e1b7684c158b54
Charlie Tatham <cnhtatham@gmail.com>
2021-03-21 16:43:21 +0000

    Created base tasks.json file which will be parsed when the webserver is activated and stored as a variable and act as the database

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+
+var tasks = JSON.parse(fs.readFileSync('tasks.json'))
 
 app.get('/', (req,res)=> { //get method
-  res.send('Hello World') //send response
+  res.send("Hello World") //send response
 })
+
+
+
 app.listen(3000)
